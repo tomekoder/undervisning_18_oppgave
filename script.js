2e1f14bddceb333e797ed31f031a2e185d271edc
tomekoder <tomekoder@gmail.com>
2024-12-04 14:02:28 +0100

    .

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 // Reference to HTML elements
-const  = document.querySelector("#");
-const  = document.querySelector("#");
-const  = document.querySelector("#");
+const inputField = document.querySelector("#inputField");
+const addButton = document.querySelector("#addButton");
+const list = document.querySelector("#list");
+const removeButton = document.querySelector("#removeButton");
+
+const outputArray = [];
 
 // Function to add a new item to the list
 function addItem() {
-  const  = .value;
+  const inputFieldValue = inputField.value;
+  console.log(inputFieldValue);
 
     // Create a new list item and give content
-    const  = document.createElement("");
-    .textContent = ;
+    const listItem = document.createElement("list");
+    listItem.textContent = inputFieldValue;
 
     // Create a remove button for the new list item
+    /*
     const  = document.createElement("");
     .textContent = "Slett";
     .classList.add("");
+    
 
     // Add an event listener to the remove button
     .addEventListener("click", function () {
       removeItem(li)
-    });
+    }); 
 
     // Append the remove button to the list item
     li.appendChild();
+    */
 
     // Append the list item to the list
-    list.appendChild();
+    list.appendChild(listItem);
 }
 
 // Function to remove an item from the list
